76ffdd3f99f69c4abe08b0c5acb891f443f50bd7
alexanderkurilenko <alex080397@gmail.com>
2017-09-30 15:47:38 +0300

    file size fixes

---
+++ SocketLocalChat/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SocketLocalChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Send = new System.Windows.Forms.Button();
+            this.IP = new System.Windows.Forms.TextBox();
+            this.ChatBox = new System.Windows.Forms.RichTextBox();
+            this.Message = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(244, 15);
+            this.Send.Margin = new System.Windows.Forms.Padding(4);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(100, 28);
+            this.Send.TabIndex = 0;
+            this.Send.Text = "Отправить";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // IP
+            // 
+            this.IP.Location = new System.Drawing.Point(0, 15);
+            this.IP.Margin = new System.Windows.Forms.Padding(4);
+            this.IP.Name = "IP";
+            this.IP.Size = new System.Drawing.Size(235, 22);
+            this.IP.TabIndex = 1;
+            this.IP.Tag = "Enter IP here";
+            this.IP.Text = "127.0.0.1";
+            this.IP.Enter += new System.EventHandler(this.IP_Enter);
+            this.IP.Leave += new System.EventHandler(this.IP_Leave);
+            // 
+            // ChatBox
+            // 
+            this.ChatBox.Location = new System.Drawing.Point(0, 47);
+            this.ChatBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.ReadOnly = true;
+            this.ChatBox.Size = new System.Drawing.Size(235, 150);
+            this.ChatBox.TabIndex = 2;
+            this.ChatBox.Text = "";
+            // 
+            // Message
+            // 
+            this.Message.Location = new System.Drawing.Point(0, 206);
+            this.Message.Margin = new System.Windows.Forms.Padding(4);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(235, 58);
+            this.Message.TabIndex = 3;
+            this.Message.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 60);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Отправить файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 265);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.ChatBox);
+            this.Controls.Add(this.IP);
+            this.Controls.Add(this.Send);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "SocketLocalChat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.TextBox IP;
+        private System.Windows.Forms.RichTextBox ChatBox;
+        private System.Windows.Forms.RichTextBox Message;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ SocketLocalChat/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace SocketLocalChat
+{
+    public partial class Form1 : Form
+    {
+        private void IP_Enter(object sender, EventArgs e)
+        {
+            if (IP.Text == (String)IP.Tag)
+            {
+                IP.Text = "";
+            }
+        }
+
+        private void IP_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(IP.Text))
+            {
+                IP.Text = (String)IP.Tag;
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Создаем поток для приема сообщений
+            new Thread(new ThreadStart(Receiver)).Start();
+            new Thread(new ThreadStart(FileReceiver)).Start();
+        }
+
+        //Метод потока
+        protected void Receiver()
+        {
+            //Создаем Listener на порт "по умолчанию"
+            TcpListener Listen = new TcpListener(7000);
+            //Начинаем прослушку
+            Listen.Start();
+            //и заведем заранее сокет
+            Socket ReceiveSocket;
+            while (true)
+            {
+                try
+                {
+                    //Пришло сообщение
+                    ReceiveSocket = Listen.AcceptSocket();
+                    Byte[] Receive = new Byte[256];
+                    //Читать сообщение будем в поток
+                    using (MemoryStream MessageR = new MemoryStream())
+                    {
+                        //Количество считанных байт
+                        Int32 ReceivedBytes;
+                        do
+                        {//Собственно читаем
+                            ReceivedBytes = ReceiveSocket.Receive(Receive, Receive.Length, 0);
+                            //и записываем в поток
+                            MessageR.Write(Receive, 0, ReceivedBytes);
+                            //Читаем до тех пор, пока в очереди не останется данных
+                        } while (ReceiveSocket.Available > 0);
+                        //Добавляем изменения в ChatBox
+                        ChatBox.BeginInvoke(AcceptDelegate, new object[] { "Received " + Encoding.Default.GetString(MessageR.ToArray()), ChatBox });
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+        }
+
+        //Метод потока
+        protected void FileReceiver()
+        {          
+            TcpListener Listen = new TcpListener(6999);
+            Listen.Start();
+            //и заведем заранее сокет
+            Socket ReceiveSocket=Listen.AcceptSocket(); 
+            NetworkStream stream=new NetworkStream(ReceiveSocket);
+            byte[] buffer = new byte[8192];
+            try
+            {
+                //stream = client.GetStream();
+                int bytesRead = stream.Read(buffer, 0, 12);
+                if (bytesRead == 0) return;
+
+                ushort id = BitConverter.ToUInt16(buffer, 0);
+                long len = BitConverter.ToInt64(buffer, 2);
+                ushort nameLen = BitConverter.ToUInt16(buffer, 10);
+                stream.Read(buffer, 0, nameLen);
+                string fileName = Encoding.UTF8.GetString(buffer, 0, nameLen);
+
+                if (id == 1)
+                {
+                    using (BinaryWriter writer = new BinaryWriter(new FileStream(fileName, FileMode.Create)))
+                    {
+                        int recieved = 0;
+                        while (recieved < len)
+                        {
+                            bytesRead = stream.Read(buffer, 0, 8192);
+                            recieved += bytesRead;
+                            writer.Write(buffer, 0, bytesRead);
+                            
+                        }
+                    }
+                   
+                }
+                
+            }
+            catch (Exception)
+            {
+                stream.Close();
+             
+            }
+            finally
+            {
+                stream.Flush();
+            }
+
+        }
+
+        /// <summary>
+        /// Отправляет сообщение в потоке на IP, заданный в контроле IP
+        /// </summary>
+        /// <param name="Message">Передаваемое сообщение</param>
+        void ThreadSend(object Message)
+        {
+              try
+            {
+                  //Проверяем входной объект на соответствие строке
+                String MessageText = "";
+                if (Message is String)
+                {
+                    MessageText = Message as String;
+                }
+                else 
+                    throw new Exception("На вход необходимо подавать строку");
+               
+                  Byte[] SendBytes = Encoding.Default.GetBytes(MessageText);
+                //Создаем сокет, коннектимся
+                IPEndPoint EndPoint = new IPEndPoint(IPAddress.Parse(IP.Text), 7000);
+                Socket Connector = new Socket(EndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                Connector.Connect(EndPoint);
+                Connector.Send(SendBytes);
+                Connector.Close();
+                //Изменяем поле сообщений (уведомляем, что отправили сообщение)
+               
+                 ChatBox.BeginInvoke(AcceptDelegate, new object[] { "Send " + MessageText, ChatBox });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+                
+           
+              
+                
+              
+            
+        }
+      
+        //Делегат доступа к контролам формы
+        delegate void SendMsg(String Text, RichTextBox Rtb);
+        
+        SendMsg AcceptDelegate = (String Text, RichTextBox Rtb) =>
+            {
+                Rtb.Text += Text + "\n";     
+            };
+
+        //Обработчик кнопки
+        private void Send_Click(object sender, EventArgs e)
+        {
+            
+            new Thread(new ParameterizedThreadStart(ThreadSend)).Start(Message.Text);          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//Отправляем файл
+            //Добавим на форму OpenFileDialog и вызовем его
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Коннектимся
+                IPEndPoint EndPoint = new IPEndPoint(IPAddress.Parse(IP.Text), 6999);
+                Socket Connector = new Socket(EndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                
+                Connector.Connect(EndPoint);
+                NetworkStream stream = new NetworkStream(Connector);
+                //Получаем имя из полного пути к файлу
+                
+                string FileName =openFileDialog1.FileName;
+                //Выделяем имя файла
+                int index = FileName.Length - 1;
+                while (FileName[index] != '\\' && FileName[index] != '/')
+                {
+                    index--;
+                }
+                //Получаем имя файла
+                String resFileName = "";
+                for (int i = index + 1; i < FileName.Length; i++)
+                    resFileName += FileName[i];
+                MessageBox.Show(openFileDialog1.FileName);
+                FileInfo file = new FileInfo(@FileName);
+
+                byte[] id = BitConverter.GetBytes((ushort)1);
+                byte[] size = BitConverter.GetBytes(file.Length);
+                byte[] fileName = Encoding.UTF8.GetBytes(file.Name);
+                byte[] fileNameLength = BitConverter.GetBytes((ushort)fileName.Length);
+                byte[] fileInfo = new byte[12 + fileName.Length];
+
+                id.CopyTo(fileInfo, 0);
+                size.CopyTo(fileInfo, 2);
+                fileNameLength.CopyTo(fileInfo, 10);
+                fileName.CopyTo(fileInfo, 12);
+
+                stream.Write(fileInfo, 0, fileInfo.Length); //Размер файла, имя
+
+                byte[] buffer = new byte[1024 * 32];
+                int count;
+
+                long sended = 0;
+
+                using (FileStream fileStream = new FileStream(file.FullName, FileMode.Open))
+                    while ((count = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        stream.Write(buffer, 0, count);
+                        sended += count;
+                        
+                    }
+                stream.Flush();            
+                Connector.Close();
+            }
+        }
+    }
+}
